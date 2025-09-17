5d65866818fe3deed829bd35d51b60536234daa2
issara-ken <kennoung@gmail.com>
2025-09-17 15:37:00 +0700

    add(): validate / globalResponse

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter'
+import { ResponseInterceptor } from './common/interceptors/response.interceptor'
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      errorHttpStatusCode: 422,
+    }),
+  )
+
+  app.useGlobalFilters(new AllExceptionsFilter())
+  app.useGlobalInterceptors(new ResponseInterceptor())
+
   const port = process.env.PORT ?? 3000
   await app.listen(port)
 
